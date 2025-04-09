26661c17b2e4783b97b6999a9c8fba8cebcd5ae6
John <johnmpalmgren@gmail.com>
2025-04-09 12:01:18 +0100

    Add side panel content - add task button and all tasks link

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,16 +1,20 @@
+import Link from 'next/link';
 import styles from './SidePanel.module.css';
+
+// user - shows logged-in user
+// add task * - modal to add task
+// view all tasks * - page with all tasks, filtering etc.
+// today's tasks - shows tasks due today
+// bin - show soft deleted tasks
 
 export default function SidePanel () {
     return(
         <>
             <div className={styles.panel}>
-                {
-                    // user - shows logged-in user
-                    // add task * - modal to add task
-                    // view all tasks * - page with all tasks, filtering etc.
-                    // today's tasks - shows tasks due today
-                    // bin - show soft deleted tasks
-                }
+                <div className={styles.wrapper}>
+                    <button className={styles.addTask}>+ Add task</button>
+                    <Link href="/">View all tasks</Link>
+                </div>
             </div>
             <div className={styles.spacer}></div>
         </>
