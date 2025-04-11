53c955db5e0bd61a84d5b3cf661c3d06e6f9df4e
John <johnmpalmgren@gmail.com>
2025-04-11 09:42:16 +0100

    add route scaffolding with dummy data

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,62 @@
+import {NextRequest, NextResponse} from "next/server";
+import {RouteParams, Task} from "@/types";
+
+const dummyData: Array<Task> = [
+    {
+        id: 0,
+        title: "task 1",
+        description: "description 1",
+        completed: false,
+        dueDate: new Date()
+    },
+    {
+        id: 1,
+        title: "task 2",
+        description: "description 2",
+        completed: false,
+        dueDate: new Date()
+    },
+    {
+        id: 2,
+        title: "task 3",
+        completed: false,
+        dueDate: new Date()
+    },
+    {
+        id: 3,
+        title: "task 4",
+        completed: true,
+        dueDate: new Date()
+    },
+    {
+        id: 4,
+        title: "task 5",
+        description: "description 5",
+        completed: true,
+        dueDate: new Date()
+    }
+]
+
+const GET = async (request: NextRequest, { params }: RouteParams) => {
+    const id = parseInt((await params).id)
+    const task = dummyData.find((task) => task.id === id);
+    if (task) {
+        return NextResponse.json(task, {status: 200, statusText: "OK"})
+    }
+    return NextResponse.json(dummyData, {status: 404, statusText: "Resource Not Found"})
+}
+
+const DELETE = async (request: NextRequest, { params }: RouteParams) => {
+    const id = parseInt((await params).id)
+    const index = dummyData.findIndex((task) => task.id === id);
+    if (index !== -1) {
+        dummyData.splice(index, 1);
+        return NextResponse.json(dummyData, {status: 200, statusText: "OK"})
+    }
+    return NextResponse.json(dummyData, {status: 404, statusText: "Resource Not Found"})
+}
+
+export {
+    GET,
+    DELETE,
+}
---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,68 @@
+import { Task } from "@/types";
+import { NextRequest, NextResponse } from 'next/server';
+
+const dummyData: Array<Task> = [
+    {
+        id: 0,
+        title: "task 1",
+        description: "description 1",
+        completed: false,
+        dueDate: new Date()
+    },
+    {
+        id: 1,
+        title: "task 2",
+        description: "description 2",
+        completed: false,
+        dueDate: new Date()
+    },
+    {
+        id: 2,
+        title: "task 3",
+        completed: false,
+        dueDate: new Date()
+    },
+    {
+        id: 3,
+        title: "task 4",
+        completed: true,
+        dueDate: new Date()
+    },
+    {
+        id: 4,
+        title: "task 5",
+        description: "description 5",
+        completed: true,
+        dueDate: new Date()
+    }
+]
+
+const GET = async () => {
+    return NextResponse.json(dummyData, {status: 200, statusText: "OK"});
+}
+
+const POST = async (request: NextRequest) => {
+    // TODO add validation
+    const newTask = await request.json();
+    newTask.id = Date.now()
+    dummyData.push(newTask);
+    return NextResponse.json(dummyData, {status: 200, statusText: "OK"});
+}
+
+const PUT = async (request: NextRequest) => {
+    // TODO add validation
+    const updatedTask = await request.json();
+    const index = dummyData.findIndex((task) => task.id === updatedTask.id);
+    if (index !== -1) {
+        dummyData[index] = updatedTask;
+        return NextResponse.json(dummyData, {status: 200, statusText: "OK"});
+    } else {
+        return NextResponse.json({error: "Task not found"}, {status: 404, statusText: "Not Found"});
+    }
+}
+
+export {
+    GET,
+    POST,
+    PUT
+};
