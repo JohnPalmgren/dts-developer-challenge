0aadbdd8b9e27bab88decd1e5f8bc1f009e5bd13
John <johnmpalmgren@gmail.com>
2025-04-13 16:22:19 +0100

    Add missing try/catch block

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -22,9 +22,17 @@ const GET = async () => {
 
 const POST = async (request: NextRequest) => {
     // TODO add validation
-    const newTask: TaskInput = await request.json();
-    const storedTask: Task = await insertTask(newTask);
-    return NextResponse.json(storedTask, {status: 200, statusText: "OK"});
+    try {
+        const newTask: TaskInput = await request.json();
+        const storedTask: Task = await insertTask(newTask);
+        return NextResponse.json(storedTask, {status: 200, statusText: "OK"});
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({error: error.message}, {status: 500, statusText: error.message});
+        } else {
+            return NextResponse.json({error: "Internal Server Error"}, {status: 500, statusText: "Internal Server Error"});
+        }
+    }
 }
 
 const PUT = async (request: NextRequest) => {
